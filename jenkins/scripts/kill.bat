49dd12aaa51062ccadf37e0b488701e2e156812c
sriram629 <maddipatisriramakrishna@gmail.com>
2024-03-06 15:15:56 +0530

    created8

--- jenkins/scripts/kill.bat
+++ jenkins/scripts/kill.bat
@@ -1,7 +1,5 @@
-#!/usr/bin/env sh
-
-echo 'The following command terminates the "npm start" process using its PID'
-echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-echo 'was executed.'
-set -x
-kill $(cat .pidfile)
+@echo off
+echo The following command terminates the "npm start" process using its PID
+echo (written to ".pidfile"), all of which were conducted when "deliver.sh"
+echo was executed.
+for /f %%i in (.pidfile) do taskkill /F /PID %%i
